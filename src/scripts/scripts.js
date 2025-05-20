d1413fd4d0a3fb8663f684761e96caaab63eafa0
PEDRO FARIA DE OLIVEIRA <CG3031977@car.ifsp>
2025-05-20 10:37:58 -0300

    nono commit

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -5,3 +5,11 @@ function continuarcadastro() {
 function irParaExplorar() {
     window.location.href = "src/explorar.html"; 
 }
+
+function toggleDescricao() {
+    const texto = document.querySelector('.descricao-texto');
+    const botao = document.querySelector('.ver-mais');
+
+    texto.classList.toggle('expandido');
+    botao.textContent = texto.classList.contains('expandido') ? 'Ver menos' : 'Ver mais';
+}
